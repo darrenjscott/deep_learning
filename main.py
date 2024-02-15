3c08055444023d666042f4635d35c043465860ed
Darren <djamesscott90@gmail.com>
2024-02-15 22:28:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(5, input_shape=(2,), activation='relu'))
+model.add(Dense(1))
+
+print(model.summary())
+
+model.compile(optimizer='adam', loss='mse')
+
+## Need to add some training and test data
+model.fit(X_test, y_test, epochs=20)
+
+print("Final loss value:",model.evaluate(X_train, y_train))
