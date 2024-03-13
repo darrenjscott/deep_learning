e12123b47b2cdbd7c0267301dd6fc7677d931b02
Darren <djamesscott90@gmail.com>
2024-03-13 22:36:24 +0000

    Model compiles. Data needs preprocessing

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from keras.models import Sequential
-from keras.layers import Dense
+import pandas as pd
+import keras
+from keras import layers
 
-model = Sequential()
-model.add(Dense(5, input_shape=(2,), activation='relu'))
-model.add(Dense(1))
 
-print(model.summary())
+cars_df = pd.read_csv('./datasets/car_prices.csv')
 
-model.compile(optimizer='adam', loss='mse')
+print(cars_df.head())
+print(cars_df.info())
+print(cars_df[['year', 'condition', 'odometer', 'mmr', 'sellingprice']])
+print(cars_df.columns)
 
-## Need to add some training and test data
-model.fit(X_test, y_test, epochs=20, validation_split=0.2)
+# We will try and see how well year, condition, and odometer can predict the selling price
+X = cars_df[['year', 'condition', 'odometer']]
+y = cars_df[['sellingprice']]
 
-print("Final loss value:",model.evaluate(X_train, y_train))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(3,)))
+model.add(layers.Dense(units=4, activation='relu'))
+model.add(layers.Dense(units=3, activation='relu'))
+model.add(layers.Dense(units=1))
+
+model.compile(optimizer='adam', loss='mae')
+
+histories = model.fit(X, y, batch_size=1000, epochs=10)
+print(histories.history['loss'])
